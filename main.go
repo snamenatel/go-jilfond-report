ee75ddafa967c2703f226591508c047e7134e5d0
Dudarek Ilya <snamenatel@gmail.com>
2021-12-25 13:24:07 +0700

    Получение конкретного отчета

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -27,14 +28,11 @@ func init() {
     }
 	token = "Bearer " + os.Getenv("TOKEN")
 	url = os.Getenv("URL")
-	reportDate = os.Args[1]
+	reportDate = getDateReport()
+	fmt.Println("Поиск отчета за период", reportDate)
 }
 
 func main() {
-	fmt.Println(url)
-	fmt.Println(token)
-	fmt.Println(reportDate)
-
 	var curentReportId string
 	for _, report := range getReportsList() {
 		if report.Name == "Внедрено " + reportDate {
@@ -42,7 +40,8 @@ func main() {
 		}
 	}
 	
-	fmt.Println(curentReportId)
+	fmt.Println("Найден отчет " + curentReportId)
+	getReport(curentReportId)
 }
 
 func getReportsList()[]ReportListItem {
@@ -52,17 +51,44 @@ func getReportsList()[]ReportListItem {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Print("Ошибка при получении списка отчетов")
+		fmt.Println("Ошибка при получении списка отчетов" + "\n")
     }
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
     if err != nil {
-        fmt.Print("Error while reading the response bytes:", err)
+        fmt.Println("Error while reading the response bytes:", err)
     }
 
 	var reporstList []ReportListItem
 	json.Unmarshal(body, &reporstList)
 
 	return reporstList
+}
+
+func getReport(id string) {
+	currentReportUrl := url + "/api/reports/" + id + "?$top=-1&fields=$type,aggregationPolicy($type,field($type,id,presentation)),attachedFields($type,field($type,aggregateable,id,presentation)),authors(fullName,id,login,ringId),bubbleWorkItems,data(duration(id,value),estimation(id,value),fieldNames,groups(duration(id,value),estimation(id,value),id,issueId,issueSummary,lines(comment,date,description,duration(id,value),estimation(id,value),fieldValues,groupName,id,issueId,issueSummary,name,totalDuration(id,value),typeDurations(duration(value),workType),userAvatarUrl,userId,userVisibleName),meta(linkedIssue(idReadable,summary),linkedUser(ringId,visibleName,postfix)),name,totalDuration(id,value),typeDurations(duration(value),workType)),totalDuration(id,value),typeDurations(duration(value),workType)),effectiveQuery,effectiveQueryUrl,grouping(id,field(id,presentation)),id,invalidationInterval,name,own,owner(id,login),pin(pinned),projects(id,name,ringId,shortName),query,range($type,from,range(id),to),sprint(id,name),status(calculationInProgress,error(id),errorMessage,isOutdated,lastCalculated,progress,wikifiedErrorMessage),updateableBy(id,name),visibleTo(id,name),wikifiedError,windowSize,workTypes(id,name)&line=issue&query=for:ringId"
+	req, err := http.NewRequest("GET", currentReportUrl, nil)
+	req.Header.Add("Authorization", token)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Ошибка при получении отчета" + "\n")
+    }
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        fmt.Println("Error while reading the response bytes:", err)
+    }
+
+	fmt.Printf(string(body))
+}
+
+func getDateReport() string {
+	if len(os.Args) < 2 {
+		return time.Now().Format("2006-01")
+	}
+	return os.Args[1]
 }
